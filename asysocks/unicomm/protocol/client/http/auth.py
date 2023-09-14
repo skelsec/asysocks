9df66531cec6ae460967b8e1de761aabdf800994
Skelsec <tamas.jos@skelsecprojects.com>
2023-09-14 22:45:48 +0200

    better SSL handling, added HTTP client

---
+++ asysocks/unicomm/protocol/client/http/auth.py
@@ -0,0 +1,169 @@
+import base64
+from hashlib import sha256
+
+from asysocks.unicomm.protocol.client.http import logger
+from asysocks.unicomm.common.target import UniProto
+from asysocks.unicomm.protocol.client.http.requestmgr import RequestManager
+
+from asyauth.common.winapi.constants import ISC_REQ
+from asyauth.common.credentials.ntlm import NTLMCredential
+from asyauth.common.credentials.kerberos import KerberosCredential
+from asyauth.common.credentials.spnego import SPNEGOCredential
+from asyauth.common.credentials.credssp import CREDSSPCredential
+
+
+class HTTPAuthManager:
+	def __init__(self, session, credential):
+		self.session = session
+		self.status = 'start'
+		self.authobj = credential.build_context()
+		self.credential = credential
+	
+	async def authenticate(self, requestmanager):
+		pass
+
+	@staticmethod
+	def from_credential(session, credential, auth_type:str = 'auto'):
+		if auth_type == 'auto':
+			if isinstance(credential, (SPNEGOCredential)) is True:
+				return HTTPAuthManagerNegotiate(session, credential)
+			elif isinstance(credential, (NTLMCredential, KerberosCredential)) is True:
+				credential = SPNEGOCredential([credential])
+				return HTTPAuthManagerNegotiate(session, credential)
+			else:
+				raise Exception('No supported auth type found!')
+		elif auth_type == 'spnego':
+			if isinstance(credential, (SPNEGOCredential)) is True:
+				return HTTPAuthManagerNegotiate(session, credential)
+			elif isinstance(credential, (NTLMCredential, KerberosCredential)) is True:
+				credential = SPNEGOCredential([credential])
+				return HTTPAuthManagerNegotiate(session, credential)
+			else:
+				raise Exception('Credential type %s cannot be used for SPNEGO authentication!' % credential.__class__.__name__)
+		elif auth_type == 'credssp':
+			if isinstance(credential, (CREDSSPCredential)) is True:
+				return HTTPAuthManagerCredSSP(session, credential)
+			elif isinstance(credential, (NTLMCredential, KerberosCredential)) is True:
+				credential = CREDSSPCredential([credential])
+				return HTTPAuthManagerCredSSP(session, credential)
+		else:
+			raise Exception('Unsupported auth type!')
+
+
+class HTTPAuthManagerCredSSP(HTTPAuthManager):
+	def __init__(self, session, credential:CREDSSPCredential):
+		HTTPAuthManager.__init__(self, session, credential)
+		self.auth_steps = False
+		self.cb_data = None
+	
+	async def authenticate(self, requestmanager):
+		self.status = 'progress'
+		authreply = None
+		domain = 'UNKNOWN'
+		for cred in self.credential.credentials:
+			domain = cred.domain
+			break
+
+		while True:
+			flags = ISC_REQ.CONNECTION|ISC_REQ.CONFIDENTIALITY|ISC_REQ.INTEGRITY
+			target_spn = 'HTTP/%s@%s' % (requestmanager.transport.target.get_hostname_or_ip(), domain)
+			authdata, to_continue, err = await self.authobj.authenticate(authreply, spn=target_spn, flags=flags) #, cb_data = self.cb_data
+			if err is not None:
+				raise err
+			
+			if to_continue is False and response.status == 200:
+				return response
+			
+			auth_header = 'CredSSP '+ base64.b64encode(authdata).decode()
+			newheaders = []
+			newheaders.append(('Authorization', auth_header))
+			for entry in requestmanager.headers:
+				newheaders.append(entry) 
+			
+			async with RequestManager(self.session, requestmanager.url, requestmanager.req_type, headers=newheaders, data=requestmanager.data, need_length=requestmanager.need_length, transport=requestmanager.transport) as response:
+				if to_continue is False and response.status != 200:
+					raise Exception('Authentication failed! %s' % response.status)
+				await response.read() #consuming all data is mandatory here!
+				if response.transport.connection is not None and response.transport.can_reuse is True:
+					self.transport = response.transport
+
+				response_auth_header = response.getheaders('www-authenticate')
+				if response_auth_header is None or len(response_auth_header) == 0:
+					if to_continue is False and response.status == 200:
+						return response
+					raise Exception('Authentication failed! No www-authenticate header found!')
+				for rauthh in response_auth_header:
+					if rauthh.startswith('CredSSP'):
+						response_auth_header = rauthh
+						break
+				else:
+					if to_continue is False and response.status == 200:
+						return response
+					raise Exception('Authentication failed! No CredSSP header found!')
+				
+
+				response_header_data_b64 = response_auth_header.replace('CredSSP ', '').strip().encode()
+				if response_header_data_b64 == b'':
+					if to_continue is False and response.status == 200:
+						return response
+					raise Exception('Authentication failed! Empty CredSSP header found!')
+				
+				authreply = base64.b64decode(response_header_data_b64)
+			
+			if to_continue is False and response.status == 200:
+				return response
+			
+class HTTPAuthManagerNegotiate(HTTPAuthManager):
+	def __init__(self, session, credential:SPNEGOCredential):
+		HTTPAuthManager.__init__(self, session, credential)
+		self.auth_steps = False
+		self.cb_data = None
+	
+	async def authenticate(self, requestmanager):
+		self.status = 'progress'
+		authreply = None
+		domain = 'UNKNOWN'
+		for cred in self.credential.credentials:
+			domain = cred.domain
+			break
+		while True:
+			flags = ISC_REQ.CONNECTION|ISC_REQ.CONFIDENTIALITY|ISC_REQ.INTEGRITY|ISC_REQ.USE_DCE_STYLE|ISC_REQ.MUTUAL_AUTH
+			if requestmanager.transport.target.protocol == UniProto.CLIENT_SSL_TCP:
+				if self.cb_data is None:
+					certdata = requestmanager.transport.connection.get_peer_certificate()
+					if certdata is not None:
+						self.cb_data = b'tls-server-end-point:' + sha256(certdata).digest()
+
+			#	flags = ISC_REQ.CONNECTION
+			target_spn = 'HTTP/%s@%s' % (requestmanager.transport.target.get_hostname_or_ip(), domain)
+			authdata, to_continue, err = await self.authobj.authenticate(authreply, spn=target_spn, flags=flags, cb_data = self.cb_data) #, 
+			if err is not None:
+				raise err
+			
+			if to_continue is False and response.status == 200:
+				logger.debug('Authentication successful!')
+				return response
+			
+			auth_header = 'Negotiate '+ base64.b64encode(authdata).decode()
+			newheaders = []
+			newheaders.append(('Authorization', auth_header))
+			for entry in requestmanager.headers:
+				newheaders.append(entry) 
+			
+			logger.debug('Updated Headers: %s' % newheaders)
+			async with RequestManager(self.session, requestmanager.url, requestmanager.req_type, headers=newheaders, data=requestmanager.data, need_length=requestmanager.need_length, transport=requestmanager.transport) as response:
+				logger.debug('Response Status: %s' % response.status )
+				logger.debug('Response Headers: %s' % response.headers)
+				
+				if to_continue is False and response.status != 200:
+					raise Exception('Authentication failed! %s' % response.status)
+				await response.read() #consuming all data is mandatory here!
+				if response.transport.connection is not None and response.transport.can_reuse is True:
+					self.transport = response.transport
+				
+				authreply = response.getheaders('www-authenticate')[0].replace('Negotiate ', '').strip().encode()
+				authreply = base64.b64decode(authreply)
+			
+			if to_continue is False and response.status == 200:
+				logger.debug('Authentication successful!')
+				return response
---
+++ asysocks/unicomm/protocol/client/http/client.py
@@ -0,0 +1,157 @@
+
+
+import re
+import copy
+import asyncio
+import http.cookiejar
+from urllib.parse import urlparse, urljoin
+from typing import List, Tuple, Any, Dict
+
+from asyauth.common.credentials import UniCredential
+from asysocks.unicomm.protocol.client.http.requestmgr import RequestManager
+from asysocks.unicomm.protocol.client.http.auth import HTTPAuthManager
+from asysocks.unicomm.protocol.client.http.commons.messages import MockHTTPRequest, HTTPResponse
+from asysocks.unicomm.protocol.client.http.commons.factory import HTTPConnectionFactory
+from asysocks.unicomm.protocol.client.http.transport import HTTPClientTransport
+
+http_url_pattern = re.compile(r'^https?:\/\/')
+
+class ClientSession:
+	def __init__(self, url:str=None, static_headers:Dict[str, str]={}, credential:UniCredential=None, auth_type='auto', ssl_ctx=None, force_sinle_connection:bool=False):
+		self.url = url
+		self.cookiejar:http.cookiejar.CookieJar = http.cookiejar.CookieJar()
+		self.factory:HTTPConnectionFactory = None
+		self.authmanager:HTTPAuthManager = None
+		self.ssl_ctx = ssl_ctx
+		self.static_headers = static_headers
+		self.force_sinle_connection = force_sinle_connection
+		self.auth_type = auth_type
+		if self.url is not None:
+			self.factory = HTTPConnectionFactory.from_url(url)
+		if credential is not None:
+			self.authmanager = HTTPAuthManager.from_credential(self, credential, auth_type=self.auth_type)
+		self.connections = []
+		self.session_closed_evt = asyncio.Event()
+	
+	async def __aenter__(self):
+		return self
+	
+	async def __aexit__(self, exc_type, exc, tb):
+		await self.close()
+	
+	async def close(self):
+		if self.session_closed_evt.is_set() is True:
+			return
+		self.session_closed_evt.set()
+		for conn in self.connections:
+			await conn.disconnect()
+		self.connections = []
+	
+	def process_url(self, url:str):
+		if bool(http_url_pattern.match(url)) is True:
+			parsed_url = urlparse(url)
+			#if self.url is not None:
+			#	temp = f"{parsed_url.scheme}://{parsed_url.netloc}/"
+			#	if temp != self.url:
+			#		raise Exception('URL was already specified!')
+			
+			self.url = url
+			self.factory = HTTPConnectionFactory.from_url(url)
+			if self.factory.get_credential() is not None:
+				self.authmanager = HTTPAuthManager.from_credential(self, self.factory.get_credential(), self.auth_type)
+
+			target = parsed_url.path
+			if target == '':
+				target = '/'
+			if target.startswith('/') is False:
+				target = '/' + target
+			if parsed_url.query != '':
+				target += parsed_url.query
+		
+		else:
+			target = url
+			if target == '':
+				target = '/'
+			if target.startswith('/') is False:
+				target = '/' + target
+			
+		original_url = urljoin(self.url, target)
+		return target, original_url
+	
+	async def get_transport(self) -> HTTPClientTransport:
+		if self.factory is None:
+			raise Exception('No URL was specified!')
+		if self.force_sinle_connection is True and len(self.connections) > 0:
+			return self.connections[0]
+		conn = HTTPClientTransport(self.factory.get_target())
+		await conn.connect()
+		self.connections.append(conn)
+		return conn
+	
+	def apply_cookies(self, url:str, headers:List[Tuple[str,str]]) -> List[Tuple[str,str]]:
+		req = MockHTTPRequest(url)
+		self.cookiejar.add_cookie_header(req)
+		for entry in req._headers:
+			headers.append((entry, req._headers[entry]))
+		return headers
+	
+	def apply_headers(self, url, headers:List[Tuple[str,str]]) -> List[Tuple[str,str]]:
+		final_headers = copy.deepcopy(headers)
+		entry_exists = lambda entry, lst: any(x[0] == entry for x in lst)
+		
+		for entry in self.static_headers:
+			if not entry_exists(entry, final_headers):
+				final_headers.append((entry, self.static_headers[entry]))
+		self.apply_cookies(url, final_headers)
+		return final_headers
+
+	def get(self, url: str, *, allow_redirects: bool = True, headers=[], **kwargs: Any) -> RequestManager:
+		return RequestManager(self, url, 'GET', allow_redirects=allow_redirects, headers=headers, **kwargs)
+
+	def post(self, url: str, *, data: bytes = None, allow_redirects: bool = True, headers=[], **kwargs: Any) -> RequestManager:
+		return RequestManager(self, url, 'POST', data=data, allow_redirects=allow_redirects, headers=headers, need_length=True, **kwargs)
+	
+
+async def amain():
+	#target = UniTarget('www.google.com', 443, protocol=UniProto.CLIENT_SSL_TCP)
+	#conn = HTTPClientTransport(target)
+	#await conn.connect()
+	#resp = await conn.get(headers=[('Connection', 'keep-alive')])
+	#print(resp.headers)
+	#await asyncio.sleep(10)
+	#x = await resp.text()
+	#await asyncio.sleep(100)
+
+	async with ClientSession() as session:
+		async with session.get('http://www.google.com/') as resp:
+			print(resp.status)
+			print(await resp.text())
+		async with session.get('http://www.google.com/') as resp:
+			print(resp.status)
+			#print(await resp.text())
+
+
+	#async with ClientSession() as session:
+	#	resp = await session.get('http://www.google.com/')
+	#	print(resp.status)
+	#	print(await resp.text())
+
+	#session = ClientSession('http://www.google.com/')
+	#resp = await session.get('/')
+	#print(resp.headers)
+	##print(await resp.text())
+	#print()
+	#print(session.cookiejar)
+	#resp = await session.get('/')
+	#print(resp.headers)
+	##print(await resp.text())
+
+def main():
+	asyncio.run(amain())
+
+if __name__ == '__main__':
+	main()
+
+
+
+	
